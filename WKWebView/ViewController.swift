07508588ca50ddea0982f69cea63b5dab2098c4c
Justin Trautman <justin@modularmobile.net>
2018-11-16 09:48:00 -0800

    Initial Commit

---
+++ WKWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WKWebView
+//
+//  Created by Justin Trautman on 11/16/18.
+//  Copyright © 2018 Justin Trautman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
